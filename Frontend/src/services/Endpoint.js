7f695c5b29ec4cc43c24ccf399a9f06340b53dbb
Kartika Kannojiya <124906223+kartika-k@users.noreply.github.com>
2025-04-29 00:33:01 +0530

    Update Endpoint.js

--- Frontend/src/services/Endpoint.js
+++ Frontend/src/services/Endpoint.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BaseUrl='http://localhost:5000'
+export const BaseUrl='https://blog-application-backend-fujt.onrender.com'
 const instance = axios.create({
     baseURL:BaseUrl,
     // headers: {
